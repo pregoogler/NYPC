8d784ef94fa5b865a81c71ea5d897a44b495098f
FNOT <41667123+FNOT@users.noreply.github.com>
2018-10-24 16:18:18 +0900

    카드 덱 + 알사탕

--- Fn0T.c
+++ Fn0T.c
@@ -5,6 +5,7 @@
 #include <math.h>
 
 // card deck problem
+/*
 int main(void) {
 	int checker[5] = { 0, 0, 0, 0, 0 }, i, testcase, result = 0;
 	char * card = malloc(sizeof(char) * 10);
@@ -46,3 +47,30 @@ int main(void) {
 
 	return 0;
 }
+*/
+
+// ballcandy problem
+/*
+int main(void) {
+	int i, value = 0;
+	char order[10000] = {0,};
+
+	scanf("%d", &value);
+
+	for (i = 0; i < value; ++i) {
+		scanf("%c", &order[i]);
+	}
+	if (value % 2 == 0) {
+		for (i = 0; i <= (value / 2); ++i) {
+			printf("%c", order[i]);
+		}
+	}
+	else {
+		for (i = 0; i <= ((value + 1) / 2); ++i) {
+			printf("%c", order[i]);
+		}
+	}
+
+	return 0;
+}
+*/
